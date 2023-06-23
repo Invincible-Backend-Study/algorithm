6c992c0c1c37a14bceddfea3591e508fa3d2e007
이호석 <66772624+HiiWee@users.noreply.github.com>
2023-06-24 03:44:36 +0900

    [백준 2748] 피보나치 수 2 - DP

---
+++ hoseok/week11/Boj2748.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 1];
+        if (n > 0) {
+            dp[1] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        bw.write(dp[n] + "");
+        bw.flush();
+        bw.close();
+    }
+}
