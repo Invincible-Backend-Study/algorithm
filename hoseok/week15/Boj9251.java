d351ee3c6d3e8c648805571ff975efce2020ed40
이호석 <66772624+HiiWee@users.noreply.github.com>
2023-07-21 04:35:20 +0900

    [백준 9251] LCS - DP2

---
+++ hoseok/week15/Boj9251.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int length1 = str1.length();
+        int length2 = str2.length();
+
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                char c1 = str1.charAt(i - 1);
+                char c2 = str2.charAt(j - 1);                
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        bw.write(dp[length1][length2] + "");
+        bw.flush();
+        bw.close();
+    }
+}
