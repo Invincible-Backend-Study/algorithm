cb39f5b8603b170e0311906a31ac9c75460b97cd
이호석 <66772624+HiiWee@users.noreply.github.com>
2023-10-23 18:25:40 +0900

    [백준 2798] 블랙잭 - 완탐

---
+++ hoseok/week28/Boj2798.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = numbers[i] + numbers[j] + numbers[k];
+                    if (maxSum < sum && sum <= m) {
+                        maxSum = sum;
+                    }
+                }
+            }
+        }
+
+        bw.write(maxSum + "");
+        bw.flush();
+        bw.close();
+    }
+}
