fdce8b3e8b7b0bb13beddff06352803b0ef68a55
이호석 <66772624+HiiWee@users.noreply.github.com>
2023-11-09 20:55:03 +0900

    [백준 15661] 링크와 스타트 - 완탐

---
+++ hoseok/week31/Boj15661.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int n, total;
+    static int[][] scores;
+    static int[] eachScoreSum;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        scores = new int[n][n];
+        eachScoreSum = new int[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                scores[i][j] = Integer.parseInt(st.nextToken());
+                total += scores[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += scores[i][j] + scores[j][i];
+            }
+            eachScoreSum[i] = sum;
+        }
+
+        combinations(0, 0);
+
+        bw.write(min + "");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void combinations(int index, int count) {
+        if (index == n) {
+            updateMinDifference();
+            return;
+        }
+        visited[index] = true;
+        combinations(index + 1, count + 1);
+        visited[index] = false;
+        combinations(index + 1, count);
+    }
+
+    public static void updateMinDifference() {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                sum += eachScoreSum[i];
+            }
+        }
+        min = Math.min(min, Math.abs(total - sum));
+    }
+}
