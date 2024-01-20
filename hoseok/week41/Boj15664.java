cec9129c9826bbd3da4e91c35f4b7240f0e4bc47
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-01-20 22:02:33 +0900

    [백준 15664] N과 M (10) - 백트래킹

---
+++ hoseok/week41/Boj15664.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int n, m;
+    static int[] input;
+    static int[] numbers;
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        input = new int[n];
+        numbers = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input);
+
+        combinations(0, 0);
+
+        bw.write(result.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void combinations(int count, int index) {
+        if (count == m) {
+            for (int number : numbers) {
+                result.append(number).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+
+        int pre = -1;
+        for (int i = index; i < n; i++) {
+            if (pre != input[i]) {
+                pre = input[i];
+                numbers[count] = input[i];
+                combinations(count + 1, i + 1);
+            }
+        }
+    }
+}
