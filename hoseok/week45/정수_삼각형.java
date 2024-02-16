ea78a61bd9712c040bf9093d30b8a05427e0a3d7
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-02-17 02:01:40 +0900

    [PRGM] 정수 삼각형 - DP

---
+++ hoseok/week45/정수_삼각형.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        int[][] dp = new int[triangle.length + 1][triangle[triangle.length - 1].length + 1];
+        dp[1][1] = triangle[0][0];
+        
+        for (int i = 2; i < dp.length; i++) {
+            for (int j = 1; j <= triangle[i - 1].length; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i - 1][j - 1];
+            }
+        }
+        
+        for (int i = 1; i < dp[0].length; i++) {
+            answer = Math.max(answer, dp[dp.length - 1][i]);
+        }
+        
+        return answer;
+    }
+}
