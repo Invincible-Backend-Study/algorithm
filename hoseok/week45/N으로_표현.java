cb00fcd432f9ebbf90e1d8290dcd45df23858c07
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-02-17 00:18:55 +0900

    [PRGM] N으로 표현 - DP

---
+++ hoseok/week45/N으로_표현.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int N, int number) {
+        Set<Integer>[] dp = new HashSet[8 + 1];
+        
+        for (int i = 0; i <= 8; i++) {
+            dp[i] = new HashSet<>();
+        }
+        
+        int answer = -1;
+        for (int k = 1; k <= 8; k++) {
+            dp[k].add(iterNumber(N, k));
+            
+            for (int i = 1; i < k; i++) {
+                for (int j = 1; j < k; j++) {
+                    if (i + j != k) {
+                        continue;
+                    }
+                    for (int number1 : dp[i]) {
+                        for (int number2 : dp[j]) {
+                            
+                            dp[k].add(number1 + number2);
+                            if (number1 - number2 > 0) {
+                                dp[k].add(number1 - number2);
+                            }
+                            if (number1 / number2 != 0) {
+                                dp[k].add(number1 / number2);
+                            }
+                            dp[k].add(number1 * number2);
+                        }
+                    }
+                }
+            }
+            if (dp[k].contains(number)) {
+                answer = k;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    public int iterNumber(int value, int count) {
+        int number = 0;
+        for (int i = 0; i < count; i++) {
+            number = number * 10 + value;
+        }
+        return number;
+    }
+}
