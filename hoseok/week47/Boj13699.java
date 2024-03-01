999a0f744f0d915549e9bb0cc0b15bcd6eff6cb3
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-03-01 23:27:16 +0900

    Create Boj13699

---
+++ hoseok/week47/Boj13699.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[36];
+
+        dp[0] = 1L;
+        dp[1] = 1L;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        bw.write(Long.toString(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+}
