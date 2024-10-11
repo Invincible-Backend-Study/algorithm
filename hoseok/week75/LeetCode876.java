455ee921d5f117aa79fb79fb282af7dc4773b6ad
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-10-11 23:55:53 +0900

    [LeetCode 876] Middle of the Linked List - Linked List

---
+++ hoseok/week75/LeetCode876.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        int size = 0;
+
+        ListNode start = head;
+        while (start != null) {
+            size++;
+            start = start.next;
+        }
+        
+        int targetSize = size / 2;
+        size = 0;
+        start = head;
+        while (start != null && size < targetSize) {
+            size++;
+            start = start.next;
+        }
+
+        return start;
+    }
+}
+
+/**
+ * 좀 더 효율적인 방식
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
