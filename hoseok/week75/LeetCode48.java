ee93b51675e52ce750361d97fdaf01c5cf09c92e
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-10-14 00:14:21 +0900

    [LeetCode 48] Rotate Image - Array

---
+++ hoseok/week75/LeetCode48.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    int n, m;
+
+    public void rotate(int[][] matrix) {
+        n = matrix.length;
+        m = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m - 1 - j];
+                matrix[i][m - 1 - j] = temp;
+            }
+        }
+    }
+}
